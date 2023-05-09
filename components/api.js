1053e794651692e082828d3aeac8bafa6ebd2935
Meo <88534564+mezoa@users.noreply.github.com>
2023-05-09 21:11:21 +0800

    Delete components directory

--- components/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const API_URL = process.env.NEXT_PUBLIC_STRAPI_URL; // Change this to your Strapi URL
-
-const api = axios.create({
-  baseURL: API_URL,
-});
-
-export default api;
